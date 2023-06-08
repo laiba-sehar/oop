ed1f16c99883435ff0c8960be700b22a7756ecf4
laiba-sehar <120155099+laiba-sehar@users.noreply.github.com>
2023-06-09 00:21:36 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,66 +1,61 @@
-﻿using System;
-using coffeetask.BL;
-using coffeetask.UI;
-
-namespace coffeetask
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace task6
 {
     class Program
     {
+        class product
+        {
+            public string name;
+            public string catagory;
+            public int price;
+            public int stock;
+            public int minimumStock;
+        }
         static void Main(string[] args)
         {
-            Coffeetask n = new Coffeetask();
-            int option;
-            double total = 0;
-            while (true)
+            product s = new product();
+            List<product> p = new List<product>();
+         
+
+        }
+        static product addProduct(product s)
+        {
+            Console.WriteLine("Enter name of the product :");
+            s.name = Console.ReadLine();
+            Console.WriteLine("Enter catagory of the product :");
+            s.catagory = Console.ReadLine();
+            Console.WriteLine("Enter price of the product :");
+            s.price = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter stock Quantity of the product :");
+            s.stock=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter minimum stock Quantity of the product :");
+            s.minimumStock = int.Parse(Console.ReadLine());
+            Console.WriteLine("Added successfully");
+            Console.WriteLine("Press any key to continue...");
+            return s;
+        }
+        static void viewProduct(List<product> p)
+        {
+            for(int i=0;i<p.Count;i++)
             {
-                Console.Clear();
-                option = UserInterface.mainMenu();
-                if (option == 1)
-                {
-                    MenuItem m = coffeetask.UI.UserInterface.addMenuItem();
-                    Coffeetask.storeInList(m);
-                    Console.ReadKey();
-                }
-                else if (option == 2)
-                {
-                    Coffeetask.viewCheapestItem();
-                    Console.ReadKey();
-                }
-                else if (option == 3)
-                {
-                    Coffeetask.viewDrinkMenu();
-                    Console.ReadKey();
-                }
-                else if (option == 4)
-                {
-                    Coffeetask.viewFoodMenu();
-                    Console.ReadKey();
-                }
-                else if (option == 5)
-                {
-                    total = Coffeetask.addOrder();
-                    Console.ReadKey();
-                }
-                else if (option == 6)
-                {
-                    Coffeetask.viewOrderInList();
-                    Console.ReadKey();
-                }
-                else if (option == 7)
-                {
-                    Coffeetask.fulfillOrder();
-                    Console.ReadKey();
-                }
-                else if (option == 8)
-                {
-                    UserInterface.dueAmount(total);
-                    Console.ReadKey();
-                }
-                else if(option == 9)
-                {
-                    break;
-                }
+                Console.Write("Name of product :");
+                Console.WriteLine(p[i].name);
+                Console.Write("catagory of product :");
+                Console.WriteLine(p[i].catagory);
+                Console.Write("price of product :");
+                Console.WriteLine(p[i].price);
+                Console.Write("stock Quantity of product :");
+                Console.WriteLine(p[i].stock);
+                Console.Write("minimum stock Quantity of product :");
+                Console.WriteLine(p[i].minimumStock);
             }
+
         }
     }
 }
